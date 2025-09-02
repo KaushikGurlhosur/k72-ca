c860c89b0d0b75fe42e89b9f2392158745541775
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-02 17:21:00 +0530

    Agency component GSAP animation - for image scroll

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -7,20 +7,76 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Agence = () => {
   const imageDivRef = useRef(null);
+  const imageRef = useRef();
+
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
+  ];
 
   useGSAP(() => {
-    gsap.to(imageDivRef.current, {
-      scrollTrigger: {
-        trigger: imageDivRef.current,
-        start: "top 36%",
-        end: "top -100%",
-        scrub: true,
-        markers: true,
-        pin: true,
+    ScrollTrigger.matchMedia({
+      "(max-width: 767px)": () => {
+        gsap.to(imageDivRef.current, {
+          scrollTrigger: {
+            trigger: imageDivRef.current,
+            start: "top 20%",
+            end: "top, -150%",
+            scrub: true,
+            pin: true,
+            onUpdate: (elem) => {
+              //   //   console.log(Math.round(elem.progress * imageArray.length));
+              const imageIndex = Math.round(
+                elem.progress * (imageArray.length - 1)
+              );
+              //   console.log(imageRef.current.src);
+              imageRef.current.src = imageArray[imageIndex];
+              //   console.log(elem.progress);
+            },
+          },
+          yPercent: 100,
+          ease: "power1.inOut",
+        });
       },
-      y: 300,
-      ease: "power1.inOut",
-      duration: 3,
+
+      // Desktop
+      "(min-width: 768px)": () => {
+        gsap.to(imageDivRef.current, {
+          yPercent: 150,
+          scrollTrigger: {
+            trigger: imageDivRef.current,
+            start: "top 36%",
+            end: "top -30%",
+            scrub: true,
+
+            pin: true,
+            onUpdate: (elem) => {
+              //   console.log(Math.round(elem.progress * imageArray.length));
+              const imageIndex = Math.round(
+                elem.progress * (imageArray.length - 1)
+              );
+              //   console.log(imageRef.current.src);
+              imageRef.current.src = imageArray[imageIndex];
+            },
+          },
+
+          ease: "power1.inOut",
+        });
+      },
     });
   }, []);
   return (
@@ -30,8 +86,9 @@ const Agence = () => {
           className="h-[20vw] w-[15vw] rounded-2xl overflow-hidden absolute top-52 left-[30vw] md:top-64 lg:top-96 xl:top-2/5 md:left-[30vw]"
           ref={imageDivRef}>
           <img
+            ref={imageRef}
             className="h-full w-full object-cover"
-            src="https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg"
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
             alt=""
           />
         </div>
