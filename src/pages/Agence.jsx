b149ce1a28b88157ae98433f2417fad32a078a91
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-02 14:00:50 +0530

    Agency component is now responsive across all screens

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,19 +1,33 @@
 const Agence = () => {
   return (
-    <div className=" ">
-      <div className="mt-[55vh]">
-        <h1 className="text-black text-[15vw] md:text-[20vw] uppercase leading-20 md:leading-[30vh] text-center font-bold font-[questrial]">
-          SEVEN7Y TWO
-        </h1>
+    <div>
+      <div className="section-1">
+        <div className="h-[20vw] w-[15vw] rounded-4xl overflow-hidden absolute top-52 left-[30vw] md:top-64 lg:top-96 xl:top-2/5 md:left-[30vw]">
+          <img
+            className="h-full w-full object-cover"
+            src="https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
       </div>
-      <div className="flex justify-center md:justify-end  md:pr-10 mt-20">
-        <p className="text-black font-[questrial] font-bold text-3xl md:text-6xl  w-[50%] tracking-wide">
-          We’re inquisitive and open-minded, and we make sure creativity crowds
-          out ego from every corner. A brand is a living thing, with values, a
-          personality and a story. If we ignore that, we can achieve short-term
-          success, but not influence that goes the distance. We bring that
-          perspective to every brand story we help tell.
-        </p>
+      <div className="section-2 h-screen">
+        <div className="font-[questrial] relative">
+          <div className="mt-[30vh] md:mt-[35vh] lg:mt-[55vh]">
+            <h1 className="text-black text-[15vw] md:text-[20vw] uppercase leading-none md:leading-none text-center font-bold font-[questrial]">
+              SEVEN7Y TWO
+            </h1>
+          </div>
+          <div className="flex justify-center md:justify-end  md:pr-10 mt-20">
+            <p className="text-black font-[questrial] font-bold text-3xl md:text-4xl  w-[60%] indent-40 tracking-widest leading-snug">
+              We’re inquisitive and open-minded, and we make sure creativity
+              crowds out ego from every corner. A brand is a living thing, with
+              values, a personality and a story. If we ignore that, we can
+              achieve short-term success, but not influence that goes the
+              distance. We bring that perspective to every brand story we help
+              tell.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
