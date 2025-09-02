40da2ab3a9c70e4bcc173c6e34b506b1175ac93c
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-02 15:06:55 +0530

    Agency component GSAP animation

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,34 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Agence = () => {
+  const imageDivRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        start: "top 36%",
+        end: "top -100%",
+        scrub: true,
+        markers: true,
+        pin: true,
+      },
+      y: 300,
+      ease: "power1.inOut",
+      duration: 3,
+    });
+  }, []);
   return (
     <div>
       <div className="section-1">
-        <div className="h-[20vw] w-[15vw] rounded-2xl overflow-hidden absolute top-52 left-[30vw] md:top-64 lg:top-96 xl:top-2/5 md:left-[30vw]">
+        <div
+          className="h-[20vw] w-[15vw] rounded-2xl overflow-hidden absolute top-52 left-[30vw] md:top-64 lg:top-96 xl:top-2/5 md:left-[30vw]"
+          ref={imageDivRef}>
           <img
             className="h-full w-full object-cover"
             src="https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg"
