61bc379145c9d1f74e61d050376c20fb0588c245
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-09-04 17:18:48 +0530

    Overlay animation

---
+++ src/components/common/Stairs.jsx
@@ -0,0 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
+import { useLocation } from "react-router-dom";
+
+const Stairs = () => {
+  const stairParentRef = useRef();
+
+  const currentPath = useLocation().pathname;
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.to(stairParentRef.current, {
+      display: "block",
+    });
+    tl.from(".stair", {
+      height: 0,
+      stagger: {
+        amount: -0.25,
+      },
+    });
+
+    tl.to(".stair", {
+      y: "100%",
+      stagger: {
+        amount: -0.25,
+      },
+    });
+
+    tl.to(stairParentRef.current, {
+      display: "none",
+    });
+  }, [currentPath]);
+
+  return (
+    <div ref={stairParentRef} className="h-screen w-full fixed z-20 top-0">
+      <div className="h-full w-full flex">
+        <div className="stair h-full w-1/5 bg-black"></div>
+        <div className="stair h-full w-1/5 bg-black"></div>
+        <div className="stair h-full w-1/5 bg-black"></div>
+        <div className="stair h-full w-1/5 bg-black"></div>
+        <div className="stair h-full w-1/5 bg-black"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Stairs;
